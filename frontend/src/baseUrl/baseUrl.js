f803b189f461234a2b9fd6642bd03a58ebe0ab51
Sneha <snehajaiswar621@gmail.com>
2025-06-18 12:25:42 +0530

    Export BASE_URL and fix import in index.js

--- frontend/src/baseUrl/baseUrl.js
+++ frontend/src/baseUrl/baseUrl.js
@@ -1,7 +1,7 @@
 
 // const baseUrl = 'http://localhost:5000/api'
 // const baseUrl = 'https://e-learning-website-7tsw.onrender.com/api'
-const BASE_URL = "https://e-learning-backend.onrender.com";
+const baseUrl = "https://e-learning-backend.onrender.com";
 
 
 export default baseUrl
