f0eb57feb59f0dc96fb82d13755bf30a6292c02c
Sneha <snehajaiswar621@gmail.com>
2025-06-23 13:24:06 +0530

    Your descriptive commit message

--- frontend/src/baseUrl/baseUrl.js
+++ frontend/src/baseUrl/baseUrl.js
@@ -2,6 +2,6 @@
 // const baseUrl = 'http://localhost:5000/api'
 // const baseUrl = 'https://e-learning-website-7tsw.onrender.com/api'
 // ✅ FIXED
-const baseUrl = "https://e-learning-backend.onrender.com/api";
+const baseUrl = "https://e-learning-website-kv7w.onrender.com/api";
 
 export default baseUrl;
